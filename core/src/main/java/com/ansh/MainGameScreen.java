3b0e5cca5c2338678417d0d91e79cdc2cba0179f
Ansh-gaur <147902097+Ansh-gaur@users.noreply.github.com>
2024-10-25 11:49:03 +0530

    Update MainGameScreen.java

--- core/src/main/java/com/ansh/MainGameScreen.java
+++ core/src/main/java/com/ansh/MainGameScreen.java
@@ -20,10 +20,15 @@ public class MainGameScreen implements Screen {
     private Texture arrowTexture;
     private ImageButton backButton;
     private Screen previousScreen;// Reference to the previous screen
-    //1
     private Catapult cata;
-    //2
     private Image cata_image;
+    private BlackBird bb;
+    private Image bb_image;
+    private GlassBlock gbl;
+    private Image gb1,gb2,gb3,gb4,gb5,gb6;
+    private PigA piga;
+    private Image pig_image;
+
 
 
     // Constructor to pass the previous screen
@@ -36,8 +41,11 @@ public class MainGameScreen implements Screen {
         // Initialize the stage and set it to handle input
         stage = new Stage(new ScreenViewport());
         Gdx.input.setInputProcessor(stage);
-        //3
         cata=new Catapult();
+        bb=new BlackBird();
+        gbl=new GlassBlock();
+        piga=new PigA();
+
 
 
         // Load the background texture
@@ -50,11 +58,57 @@ public class MainGameScreen implements Screen {
 
         // Add the background image to the stage
         stage.addActor(backgroundImage);
-        //4
+        //Catapult
         cata_image=new Image(cata.cataimg);
         cata_image.setSize(Gdx.graphics.getWidth() / 10, (Gdx.graphics.getHeight()+1000) / 10);
         cata_image.setPosition(30,10);
         stage.addActor(cata_image);
+        //blackbird
+
+        bb_image=new Image(bb.blackimg);
+        bb_image.setSize((Gdx.graphics.getWidth()) / 10, Gdx.graphics.getHeight() / 10);
+        bb_image.setPosition(30,100);
+        stage.addActor(bb_image);
+
+        //structure of glass
+        gb1=new Image(gbl.glassimg);
+        gb1.setSize(Gdx.graphics.getWidth() / 10, Gdx.graphics.getHeight() / 10);
+        gb1.setPosition(500,10);
+        stage.addActor(gb1);
+
+        gb2=new Image(gbl.glassimg);
+        gb2.setSize(Gdx.graphics.getWidth() / 10, Gdx.graphics.getHeight() / 10);
+        gb2.setPosition(500,gb1.getHeight()+3);
+        stage.addActor(gb2);
+
+        gb3=new Image(gbl.glassimg);
+        gb3.setSize(Gdx.graphics.getWidth() / 10, Gdx.graphics.getHeight() / 10);
+        gb3.setPosition(500,2*(gb1.getHeight()+3));
+        stage.addActor(gb3);
+
+        gb4=new Image(gbl.glassimg);
+        gb4.setSize(Gdx.graphics.getWidth() / 10, Gdx.graphics.getHeight() / 10);
+        gb4.setPosition(500,3*(gb1.getHeight()+3));
+        stage.addActor(gb4);
+
+        gb5=new Image(gbl.glassimg);
+        gb5.setSize(Gdx.graphics.getWidth() / 10, Gdx.graphics.getHeight() / 10);
+        gb5.setPosition(500,4*(gb1.getHeight()+2));
+        stage.addActor(gb5);
+
+        gb6=new Image(gbl.glassimg);
+        gb6.setSize(Gdx.graphics.getWidth() / 10, Gdx.graphics.getHeight() / 10);
+        gb6.setPosition(500,5*(gb1.getHeight()+2));
+        stage.addActor(gb6);
+
+        pig_image=new Image(piga.pigApic);
+        pig_image.setSize(Gdx.graphics.getWidth()/10, Gdx.graphics.getHeight()/10);
+        pig_image.setPosition(500,6*(gb1.getHeight()+2));
+        stage.addActor(pig_image);
+
+
+
+
 
         // Load the arrow image texture
         arrowTexture = new Texture("arrowmain.jpg"); // Replace with your arrow image file
